7acf075070f8d95f46ce3323b17b15e932b51adf
Benedict Odoh <benedict.ik98@gmail.com>
2024-05-02 14:36:38 +0100

    Changed widget-themed to class-themed

--- lib/widgets/SplashScreen.dart
+++ lib/widgets/SplashScreen.dart
@@ -1,6 +1,5 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
+import 'package:chronicles/widgets/HomeScreen.dart';
 
 const double minimumPadding = 5.0;
 
@@ -11,65 +10,73 @@ Widget getSplashScreen() {
     width: 200.0,
     height: 200.0,
   );
+  return image;
+}
 
-  return Padding(
-    padding: EdgeInsets.symmetric(horizontal: 35.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(height: minimumPadding * 15),
-        image,
-        SizedBox(height: minimumPadding * 5),
-        Text(
-          "Nothing’s Here 🙁",
-          style: TextStyle(
-            fontSize: 30,
-            decoration: TextDecoration.none,
-            color: Color(0xFF3A3A3A),
-          ),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: minimumPadding * 5),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Text(
-            "Add a new note to record an event, a memory or a lingering thought",
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 35.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: minimumPadding * 15),
+          getSplashScreen(),
+          SizedBox(height: minimumPadding * 5),
+          Text(
+            "Nothing’s Here 🙁",
             style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.normal,
+              fontSize: 30,
               decoration: TextDecoration.none,
               color: Color(0xFF3A3A3A),
             ),
             textAlign: TextAlign.center,
           ),
-        ),
-        SizedBox(height: minimumPadding * 5),
-        // Wrap ElevatedButton with Expanded
-        Row(
-          children: [
-            Expanded(
-              child: ElevatedButton(
-                onPressed: () {
-                  // print("Button pressed");
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomeScreen(),
-                      ));
-                },
-                child: Text("Create a new note",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    )),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFFFBC103),
-                    foregroundColor: Color(0xFF000000)),
+          SizedBox(height: minimumPadding * 5),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              "Add a new note to record an event, a memory or a lingering thought",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+                decoration: TextDecoration.none,
+                color: Color(0xFF3A3A3A),
               ),
+              textAlign: TextAlign.center,
             ),
-          ],
-        )
-      ],
-    ),
-  );
+          ),
+          SizedBox(height: minimumPadding * 5),
+          // Wrap ElevatedButton with Expanded
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // print("Button pressed");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeScreen(),
+                        ));
+                  },
+                  child: Text("Create a new note",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      )),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFFBC103),
+                      foregroundColor: Color(0xFF000000)),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
